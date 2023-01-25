5907e5f5d5a827589ab0a74b3b3eb65165f9b5f8
Marcus Sandi <tradedomarcus@gmail.com>
2023-01-25 16:03:33 -0300

    feat: add imports of components

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,6 +1,15 @@
 import { FlexBox } from './Flexbox/flexbox'
 import { Container } from './Container/container'
+import { PokedexView } from './PokedexView/pokedexview'
+import Button from './Button/button'
+import Card from './Card/card'
+import Header from './Header/header'
+import InitialPokemons from './InitialPokemon/initialPokemon'
+import Input from './Input/input'
+import Loading from './Loading/loading'
+import PokemonCount from './PokemonsCount/pokemonsCount'
+import SinglePokemon from './SinglePokemon/singlePokemon'
+import PokemonsSection from './PokemonSection/pokemonSection'
 
 
-export { Container, FlexBox }
-// export type { IFlexBoxProps, TAlignItems, TJustifyContent }
+export { Container, FlexBox, PokedexView, Button, Card, Header, InitialPokemons, Input, Loading, PokemonCount, SinglePokemon, PokemonsSection }
